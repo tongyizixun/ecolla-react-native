6b41e77b941b70de09b0bfe0c2c4f5d8051639cf
qq382724935 <382724935@qq.com>
2021-07-22 11:45:05 +0800

    chore: 配置文件上传
    更新配置文件

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: 刘利军
+ * @Date: 2021-07-22 09:06:35
+ * @LastEditors: 刘利军
+ * @LastEditTime: 2021-07-22 09:06:53
+ * @Description:
+ * @PageName:
+ */
+
+module.exports = {
+  extends: ['cz'],
+  rules: {
+    // must add these rules
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+  },
+};
